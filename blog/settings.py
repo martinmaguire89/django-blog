d5fe5b2138d7eecbd7622fb889541acb35aaaef0
martinmaguire89 <Martin.maguire89@hotmail.co.uk>
2020-04-19 13:38:24 +0000

    Added Heroku and Postgres support for deployment

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 if os.path.exists('env.py'):
     import env
@@ -23,8 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z(q5&c83&)&$(a^nio=@x=qao_6(!5j9uiqz&#ox(mylj6@^h$'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'blog.urls'
@@ -80,12 +81,18 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    print("Postgres URL not found, using sqlite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
@@ -126,6 +133,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
--- env.py
+++ env.py
@@ -1,3 +1,5 @@
 import os
 
-os.environ.setdefault("SECRET_KEY", 'z(q5&c83&)&$(a^nio=@x=qao_6(!5j9uiqz&#ox(mylj6@^h$')
+os.environ.setdefault("SECRET_KEY", "'z(q5&c83&)&$(a^nio=@x=qao_6(!5j9uiqz&#ox(mylj6@^h$'")
+
+os.environ.setdefault("DATABASE_URL", "postgres://nvsdvroovyeasb:af38ca510b337dbaed44906a5df94b8d2d8c299c12bd657ab4f8f8f3bf296574@ec2-54-247-118-139.eu-west-1.compute.amazonaws.com:5432/db6ubl289jqf3v")
